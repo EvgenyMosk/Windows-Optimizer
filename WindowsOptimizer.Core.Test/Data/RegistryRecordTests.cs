8f27985fd282f475913e285d6c881d6356e1057b
Evgeny Moskovchenko <47689225+EvgenyMosk@users.noreply.github.com>
2020-12-29 22:29:45 +0200

    UTs for RegistryRecord.ConvertStringToRegistryKey

---
+++ WindowsOptimizer.Core.Test/Data/RegistryRecordTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Win32;
+
+namespace WindowsOptimizer.Core.Data.Tests {
+	[TestClass()]
+	public class RegistryRecordTests {
+		[TestMethod()]
+		public void ConvertStringToRegistryKey_NotSupportedRegistryRoot_ExpectArgumentException() {
+			string root = "HKEY_iMaGiNeD_rOoT_tYpE";
+			RegistryKey registryKey = null;
+
+			bool exceptionWasTrown = false;
+
+			try {
+				registryKey = RegistryRecord.ConvertStringToRegistryKey(root);
+			} catch (ArgumentException) {
+				exceptionWasTrown = true;
+			}
+
+			Assert.IsTrue(exceptionWasTrown);
+			Assert.IsNull(registryKey);
+		}
+
+		[TestMethod()]
+		public void ConvertStringToRegistryKey_LocalMachineRootStringLowerCase_ExpectLocalMachineRootObj() {
+			string root = "hkey_local_machine";
+			RegistryKey expectedRegistryKey = Registry.LocalMachine;
+
+			RegistryKey actualRegistryKey = RegistryRecord.ConvertStringToRegistryKey(root);
+
+			Assert.AreEqual(expectedRegistryKey, actualRegistryKey);
+		}
+
+		[TestMethod()]
+		public void ConvertStringToRegistryKey_LocalMachineRootStringUpperCase_ExpectLocalMachineRootObj() {
+			string root = "HKEY_LOCAL_MACHINE";
+			RegistryKey expectedRegistryKey = Registry.LocalMachine;
+
+			RegistryKey actualRegistryKey = RegistryRecord.ConvertStringToRegistryKey(root);
+
+			Assert.AreEqual(expectedRegistryKey, actualRegistryKey);
+		}
+
+		[TestMethod()]
+		public void ConvertStringToRegistryKey_CurrentUserRootString_ExpectCurrentUserRootObj() {
+			string root = "HKEY_CURRENT_USER";
+			RegistryKey expectedRegistryKey = Registry.CurrentUser;
+
+			RegistryKey actualRegistryKey = RegistryRecord.ConvertStringToRegistryKey(root);
+
+			Assert.AreEqual(expectedRegistryKey, actualRegistryKey);
+		}
+
+		[TestMethod()]
+		public void ConvertStringToRegistryKey_ClassesRootString_ExpectClassesRootObj() {
+			string root = "hkey_CLASSES_root";
+			RegistryKey expectedRegistryKey = Registry.ClassesRoot;
+
+			RegistryKey actualRegistryKey = RegistryRecord.ConvertStringToRegistryKey(root);
+
+			Assert.AreEqual(expectedRegistryKey, actualRegistryKey);
+		}
+	}
+}
--- WindowsOptimizer.Core/Data/RegistryRecord.cs
+++ WindowsOptimizer.Core/Data/RegistryRecord.cs
@@ -1,123 +1,121 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
-using System.Text;
 
 using Microsoft.Win32;
 
 namespace WindowsOptimizer.Core.Data {
-    public class RegistryRecord : IRegistryRecord {
-        #region Properties and Fields
-        public RegistryKey Root { get; }
-        public string Key { get; }
-        public string ValueName { get; }
-        public object Value { get; }
-        public RegistryValueKind ValueKind { get; }
+	public class RegistryRecord : IRegistryRecord {
+		#region Properties and Fields
+		public RegistryKey Root { get; }
+		public string Key { get; }
+		public string ValueName { get; }
+		public object Value { get; }
+		public RegistryValueKind ValueKind { get; }
 
-        #endregion
-        #region Constructors
-        public RegistryRecord(RegistryKey root, string key, string valueName, object value, RegistryValueKind kind = RegistryValueKind.DWord) {
-            Root = root;
-            Key = key;
-            ValueName = valueName;
-            Value = value;
-            ValueKind = kind;
-        }
+		#endregion
+		#region Constructors
+		public RegistryRecord(RegistryKey root, string key, string valueName, object value, RegistryValueKind kind = RegistryValueKind.DWord) {
+			Root = root;
+			Key = key;
+			ValueName = valueName;
+			Value = value;
+			ValueKind = kind;
+		}
 
-        public RegistryRecord(string root, string key, string valueName, object value, RegistryValueKind kind = RegistryValueKind.DWord) {
-            Root = ConvertStringToRegistryKey(root);
-            Key = key;
-            ValueName = valueName;
-            Value = value;
-            ValueKind = kind;
-        }
-        #endregion
+		public RegistryRecord(string root, string key, string valueName, object value, RegistryValueKind kind = RegistryValueKind.DWord) {
+			Root = ConvertStringToRegistryKey(root);
+			Key = key;
+			ValueName = valueName;
+			Value = value;
+			ValueKind = kind;
+		}
+		#endregion
 
-        public static RegistryKey ConvertStringToRegistryKey(string root) {
-            if (string.IsNullOrEmpty(root.Trim())) {
-                throw new ArgumentNullException(nameof(root));
-            }
+		public static RegistryKey ConvertStringToRegistryKey(string root) {
+			if (string.IsNullOrEmpty(root.Trim())) {
+				throw new ArgumentNullException(nameof(root));
+			}
 
-            if (root.ToUpper() == "HKEY_LOCAL_MACHINE") {
-                return Registry.LocalMachine;
-            }
-            if (root.ToUpper() == "HKEY_CURRENT_USER") {
-                return Registry.CurrentUser;
-            }
-            if (root.ToUpper() == "HKEY_CLASSES_ROOT") {
-                return Registry.ClassesRoot;
-            }
+			if (root.ToUpper() == "HKEY_LOCAL_MACHINE") {
+				return Registry.LocalMachine;
+			}
+			if (root.ToUpper() == "HKEY_CURRENT_USER") {
+				return Registry.CurrentUser;
+			}
+			if (root.ToUpper() == "HKEY_CLASSES_ROOT") {
+				return Registry.ClassesRoot;
+			}
 
-            throw new ArgumentException("This type is currently not supported!", nameof(root));
-        }
+			throw new ArgumentException("This root type is currently not supported!", nameof(root));
+		}
 
-        public override string ToString() {
-            return Root + "\\" + Key + ", " + ValueName + "=" + Value + " (" + ValueKind.ToString() + ")";
-        }
+		public override string ToString() {
+			return Root + "\\" + Key + ", " + ValueName + "=" + Value + " (" + ValueKind.ToString() + ")";
+		}
 
-        public override bool Equals(object obj) {
-            if (obj == null) {
-                return false;
-            }
+		public override bool Equals(object obj) {
+			if (obj == null) {
+				return false;
+			}
 
-            if (ReferenceEquals(this, obj)) {
-                return true;
-            }
+			if (ReferenceEquals(this, obj)) {
+				return true;
+			}
 
-            if (GetType() != obj.GetType()) {
-                return false;
-            }
+			if (GetType() != obj.GetType()) {
+				return false;
+			}
 
-            return Equals(obj as RegistryRecord);
-        }
+			return Equals(obj as RegistryRecord);
+		}
 
-        public override int GetHashCode() {
-            return base.GetHashCode();
-        }
+		public override int GetHashCode() {
+			return base.GetHashCode();
+		}
 
-        public bool Equals(IRegistryRecord other) {
-            if (other == null) {
-                return false;
-            }
+		public bool Equals(IRegistryRecord other) {
+			if (other == null) {
+				return false;
+			}
 
-            if (ReferenceEquals(this, other)) {
-                return true;
-            }
+			if (ReferenceEquals(this, other)) {
+				return true;
+			}
 
-            if (GetType() != other.GetType()) {
-                return false;
-            }
+			if (GetType() != other.GetType()) {
+				return false;
+			}
 
-            //IList<int> comparisonResult = new List<int>();
-            //IList<PropertyInfo> thisProperties = GetType().GetProperties().ToList();
-            //IList<PropertyInfo> otherProperties = other.GetType().GetProperties().ToList();
+			//IList<int> comparisonResult = new List<int>();
+			//IList<PropertyInfo> thisProperties = GetType().GetProperties().ToList();
+			//IList<PropertyInfo> otherProperties = other.GetType().GetProperties().ToList();
 
-            //if (thisProperties.Count != otherProperties.Count) {
-            //    return false;
-            //}
+			//if (thisProperties.Count != otherProperties.Count) {
+			//    return false;
+			//}
 
-            IList<int> comparisonResult = new List<int> {
-                string.Compare(Root.ToString(), other.Root.ToString()),
-                string.Compare(Key,other.Key),
-                string.Compare(ValueName,other.ValueName),
-                string.Compare(Value.ToString(),other.Value.ToString()),
-                string.Compare(ValueKind.ToString(),other.ValueKind.ToString())
-            };
+			IList<int> comparisonResult = new List<int> {
+				string.Compare(Root.ToString(), other.Root.ToString()),
+				string.Compare(Key,other.Key),
+				string.Compare(ValueName,other.ValueName),
+				string.Compare(Value.ToString(),other.Value.ToString()),
+				string.Compare(ValueKind.ToString(),other.ValueKind.ToString())
+			};
 
-            if (comparisonResult.Sum() == 0) {
-                return true;
-            } else {
-                return false;
-            }
-        }
+			if (comparisonResult.Sum() == 0) {
+				return true;
+			} else {
+				return false;
+			}
+		}
 
-        public static bool operator ==(RegistryRecord recordLeft, RegistryRecord recordRight) {
-            return ReferenceEquals(recordLeft, recordRight);
-        }
+		public static bool operator ==(RegistryRecord recordLeft, RegistryRecord recordRight) {
+			return ReferenceEquals(recordLeft, recordRight);
+		}
 
-        public static bool operator !=(RegistryRecord recordLeft, RegistryRecord recordRight) {
-            return ReferenceEquals(recordLeft, recordRight);
-        }
-    }
+		public static bool operator !=(RegistryRecord recordLeft, RegistryRecord recordRight) {
+			return ReferenceEquals(recordLeft, recordRight);
+		}
+	}
 }
