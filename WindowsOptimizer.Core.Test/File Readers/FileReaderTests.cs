b98b7d467371c18073596176a0baf4d35c95a4e6
Evgeny Moskovchenko <47689225+EvgenyMosk@users.noreply.github.com>
2021-01-08 22:03:58 +0200

    Unit tests for FileReader.ReadFromFile() (only checking argumentNullException)

---
+++ WindowsOptimizer.Core.Test/File Readers/FileReaderTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WindowsOptimizer.Core.File_Readers.Tests {
+	[TestClass()]
+	public class FileReaderTests {
+		private FileReader fileReader_testSubject;
+
+		[TestInitialize()]
+		public void TestInit() {
+			fileReader_testSubject = new FileReader();
+		}
+
+		#region ReadFromFile tests
+		[TestMethod()]
+		public void ReadFromFile_NullString_ExpectFalse() {
+			string pathToFile = null;
+			bool exceptionWasThrown = false;
+
+			try {
+				fileReader_testSubject.ReadFromFile(pathToFile);
+			} catch (ArgumentNullException) {
+				exceptionWasThrown = true;
+			}
+
+			Assert.IsTrue(exceptionWasThrown);
+		}
+
+		[TestMethod()]
+		public void ReadFromFile_EmptyString_ExpectFalse() {
+			string pathToFile = string.Empty;
+			bool exceptionWasThrown = false;
+
+			try {
+				fileReader_testSubject.ReadFromFile(pathToFile);
+			} catch (ArgumentNullException) {
+				exceptionWasThrown = true;
+			}
+
+			Assert.IsTrue(exceptionWasThrown);
+		}
+
+		[TestMethod()]
+		public void ReadFromFile_Whitespace_ExpectFalse() {
+			string pathToFile = @" ";
+			bool exceptionWasThrown = false;
+
+			try {
+				fileReader_testSubject.ReadFromFile(pathToFile);
+			} catch (ArgumentNullException) {
+				exceptionWasThrown = true;
+			}
+
+			Assert.IsTrue(exceptionWasThrown);
+		}
+		#endregion
+	}
+}
